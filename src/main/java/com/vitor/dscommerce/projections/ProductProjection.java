89f5699b4b72e9373ca534f9036230ca2d4b161e
vitorhugosdc <vitorvitinho9900@hotmail.com>
2024-07-04 09:17:41 -0300

    apenas comentários

--- src/main/java/com/vitor/dscommerce/projections/ProductProjection.java
+++ src/main/java/com/vitor/dscommerce/projections/ProductProjection.java
@@ -7,7 +7,12 @@ package com.vitor.dscommerce.projections;
 public interface ProductProjection {
 
     /*Colocamos o nome dos campos que quisermos nas formas dos métodos GET.
-     * Aqui, como queremos todos os dados dos produtos (coluna id,name,description,price e imgurl no BD e atributo id,name,description,price e imgurl da classe)*/
+     * Aqui, como queremos todos os dados dos produtos (coluna id,name,description,price e imgurl no BD e atributo id,name,description,price e imgurl da classe)
+     *
+     * SELECT * FROM tb_product significa que temos que colocar todos atributos, lá no banco vai ser
+     * tb_product.id, tb_product.name, tb_product.description... por isso o get tem que corresponder, ou seja
+     * tb_product.price tem que por getPrice(), pois é o nome do atributo no banco de dados
+     * Agora, se quisermos personalizar, usa o alias, ou seja, tb_product.price AS product_price por exemplo, ai usaria getProductPrice()*/
     Long getId();
 
     String getName();
--- src/main/java/com/vitor/dscommerce/repositories/ProductRepository.java
+++ src/main/java/com/vitor/dscommerce/repositories/ProductRepository.java
@@ -41,4 +41,32 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     @Query("SELECT obj FROM Product obj " +
             "WHERE UPPER(obj.name) LIKE CONCAT('%', UPPER(:name), '%')")
     Page<Product> searchByNameJPQL(String name, Pageable pageable);
+
+    /*ABAIXO SÃO COMENTÁRIOS DA AULA LAZY EXPLICANDO MAIS SOBRE SPRING DATA JPA E SEU FUNCIONAMENTO PARA 2 CLASSES QUE NÃO ESTÃO NO PROJETO ATUAL, MAS É BOA EXPLICAÇÃO
+    *
+
+     */
+    /*A cláusula JOIN FETCH não funciona em buscas paginadas do Sring*/
+    /*Na JPQL a gente tem que dar apelido aos OBJETOS (obj)
+     * Ao invés do nome da tabela, a gente informa o nome da classe (Employee)
+     * Ao invés da classe ou tabela da associação, colocamos o nome do atributo (obj.department)
+     *
+     * Estamos na consulta buscando todos Employee e já buscando ao mesmo tempo o departamento dele, tudo na mesma consulta
+     *
+     * Por que foi necessária essa consulta sendo que Department é para-um e quando se consulta ele por ser 1 já vem direto?
+     * Pois quando fazemos uma consulta buscando uma coleção de objetos, mesmo que eles tenham relacionamento para-um, a busca desses objetos vai ser lazy
+     * então, ele busca todos os funcionários (pois viu que era coleção), ai quando viu que precisava dos departamentos, ele foi buscando 1 por 1
+     * Ele não faz buscas repetidas, então por ex, como temos 3 departamentos, ele além da consulta de todos funcionários, faz +3 consultas, uma pra cada departamento
+     * ele não busca 14 vezes (número de funcionários) pois ele tem CACHE, então se ele já buscou 1 departamento, não vai buscar ele de novo, só um novo
+     * ele associa o mesmo departamento a todos os funcionários sem precisar de novas buscas (na mesma sesão JPA, oq significa que é do inicio ao fim da requisição,
+     * spring.jpa.open-in-view=false é só até retornar do serviço, então ela dura na conversa entre service/repository) e é finalizado antes de chegar no controller*/
+    //@Query("SELECT obj FROM Employee obj JOIN FETCH obj.department")
+    //List<Employee> findEmployeesWithDepartments();
+
+    /*Isso é um Query method, ele monta a consulta somente com o nome do método
+     *
+     * Mais opções: https://docs.spring.io/spring-data/jpa/reference/jpa/query-methods.html
+     *
+     * Ideal é usar JPQL ou SQL nativo mesmo, pois essa simples consulta abaixo acessa 4 vezes o banco de dados quando poderia ser só 1 vez*/
+    //List<Employee> findByNameContainingIgnoreCase(String name);*/
 }
