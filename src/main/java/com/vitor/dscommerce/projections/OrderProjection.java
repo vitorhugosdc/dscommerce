32bc9931843c0427d6b3a88e673a13e2b14a2a2f
vitorhugosdc <vitorvitinho9900@hotmail.com>
2024-07-08 08:25:17 -0300

    Adicionando mais comentários explicando como seria uma OrderProjection utilizando de exemplo o dscatalog

--- src/main/java/com/vitor/dscommerce/projections/OrderProjection.java
+++ src/main/java/com/vitor/dscommerce/projections/OrderProjection.java
@@ -4,6 +4,9 @@ import com.vitor.dscommerce.entities.OrderStatus;
 
 import java.time.Instant;
 
+/*Exemplo de como seria uma projeção para um Order
+* Projeções dentro de projeções são possíveis ao utilizar JPQL, como por exemplo
+* os dados do produto como id, name, price, quantity seriam uma outra projection de Product*/
 public interface OrderProjection {
 
     Long getId();
@@ -13,6 +16,16 @@ public interface OrderProjection {
     String getClientName();
     Long getPaymentId();
     Instant getPaymentMoment();
+
+    /*List<ProductProjection> getProducts() ou getItems() seria perfeitamente possível ao utilizar JPQL, assim como List<ClientMinProjection> getClient()...
+    * utilizando uma projeção dentro de outra projeção
+    * Como não é possível projeções dentro de projeções ao utilizar consultas NATIVAS, teria que fazer igual essa projection aqui, trazer tudo e ir instanciando cada cliente e produto
+    * e depois utilizando um Map assim como foi feito no dscatalog na classe ProductService no método searchProductsWithCategories
+    * Olhar também o repository, no qual teve o DISTINCT para retornar a contagem certa, pois aqui também daria problema, pois um pedido tem mais de 1 item (produto), então
+    * contaria várias vezes um mesmo pedido, mas ai utilizariamos o DISTINCT para pegar somente os pedidos diferentes
+    *
+    * https://stackoverflow.com/questions/48995744/java-spring-projection-inside-projection*/
+
     Long getProductId();
     String getProductName();
     Double getProductPrice();
